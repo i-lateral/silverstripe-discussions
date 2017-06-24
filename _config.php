8bd9642b7b4ecd6c410872966c43238636088042
Morven Lewis-Everley <morven@i-lateral.com>
2017-06-24 11:36:30 +0100

    Switch to YML config for comments

--- _config.php
+++ _config.php
@@ -1,11 +1,1 @@
 <?php
-
-if (ClassInfo::exists('Commenting')) {
-    
-    // Add commenting to discussions
-    Commenting::add('Discussion', array(
-        "require_login" => true,
-        "required_permission" => "DISCUSSIONS_POSTING",
-        "order_comments_by" => "\"Created\" ASC",
-    ));
-}
